300aa6358ad73c3513e6b794d42f11b00c0bd911
V.Dinev <vassildinev@gmail.com>
2015-10-31 15:10:27 +0200

    Added Friday Oct 30th workshop Jedi Meditation solution

---
+++ 00.Workshops/01.Oct30/JediMeditation/JediMeditation/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numberOfJedi = int.Parse(Console.ReadLine());
+
+            string[] jedi = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var masters = new HashSet<string>();
+            var knights = new HashSet<string>();
+            var padawans = new HashSet<string>();
+
+            for (int i = 0; i < jedi.Length; i++)
+            {
+                string currentJedi = jedi[i];
+                char jediLevel = currentJedi[0];
+                switch (jediLevel)
+                {
+                    case 'm':
+                        masters.Add(currentJedi);
+                        break;
+                    case 'k':
+                        knights.Add(currentJedi);
+                        break;
+                    case 'p':
+                        padawans.Add(currentJedi);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", masters) + " " + string.Join(" ", knights) + " " + string.Join(" ", padawans));
+        }
+    }
+}
